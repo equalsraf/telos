c779af91fd3f43e973fc73521b5a7111b71cdfea
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-01-27 00:02:42 +0000

    travis: Set RUST_BACKTRACE

--- contrib/travis-run.sh
+++ contrib/travis-run.sh
@@ -3,7 +3,6 @@ set -ev
 
 export LIBTLS_LIBRARY_PATH=${PWD}/deps/lib
 export LIBTLS_INCLUDE_PATH=${PWD}/deps/include
-
 export LIBTLS_LINKAGE=static
+export RUST_BACKTRACE=1
 cargo test --verbose --manifest-path tls/Cargo.toml
-
