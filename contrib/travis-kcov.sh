251c0c482cdd667518ea2b65c2d9a56fe51d4c88
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-01-27 11:10:37 +0000

    travis: Filter /toolchains/ in kcov

--- contrib/travis-kcov.sh
+++ contrib/travis-kcov.sh
@@ -3,7 +3,7 @@ set -ev
 
 # Run tests by hand with kcov
 KCOV=${PWD}/deps/bin/kcov
-KCOV_ARGS="--coveralls-id=${TRAVIS_JOB_ID} --exclude-pattern=/tests/,/crypto/,/ssl/ ../kcov-out"
+KCOV_ARGS="--coveralls-id=${TRAVIS_JOB_ID} --exclude-pattern=/tests/,/crypto/,/ssl/,/toolchains/ ../kcov-out"
 
 # Somes tests need to be in the crate folder
 pushd tls
